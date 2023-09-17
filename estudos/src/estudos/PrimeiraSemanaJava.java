7832c78b02ea2f97f30875548b07fb2f58009be3
KrlIgnacio <karolainesignacio@gmail.com>
2023-09-17 15:00:57 -0300

    Adicionei duas linhas ao código.

--- estudos/src/estudos/PrimeiraSemanaJava.java
+++ estudos/src/estudos/PrimeiraSemanaJava.java
@@ -3,7 +3,9 @@ package estudos;
 public class PrimeiraSemanaJava {
 
 	public static void main(String[] args) {
-			System.out.println("Integrando o GitHub na IDE");
+			System.out.println("Integrando o GitHub na IDE Eclipse!");
+			System.out.println("Bora aprender Java?!!");
+			System.out.println("Aprender a programare e a usar Github.");
 
 	}
 
